802ccab3e8c3c501b67a61b961309a0f04ffdabf
desertsniper87 <torshobuet@gmail.com>
2020-09-08 18:28:41 +0600

    minor string changes

--- src/main/java/com/github/newsPipeline/kafka/Producer.java
+++ src/main/java/com/github/newsPipeline/kafka/Producer.java
@@ -16,7 +16,7 @@ public class Producer {
         final Logger logger = LoggerFactory.getLogger(Producer.class);
 
         Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapservers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
@@ -33,7 +33,7 @@ public class Producer {
                             "Timestamp: " + recordMetadata.timestamp());
 //                    recordMetadata.
                 } else {
-
+                    logger.error("Error while producing");
                 }
             }
         });
