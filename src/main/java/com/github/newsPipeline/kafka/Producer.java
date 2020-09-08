14ff2b8be9a088afb05d9465c1e2d77ec26e25d3
desertsniper87 <torshobuet@gmail.com>
2020-09-08 18:27:21 +0600

    Producer class

---
+++ src/main/java/com/github/newsPipeline/kafka/Producer.java
@@ -0,0 +1,43 @@
+package com.github.newsPipeline.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class Producer {
+    private static Object StringSerializer;
+
+    public static void main(String[] args) {
+        String bootstrapservers = "127.0.0.1:9092";
+
+        final Logger logger = LoggerFactory.getLogger(Producer.class);
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world");
+        producer.send(record, new Callback() {
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                if (e!=null) {
+                    logger.info("Received new metadata. \n" +
+                            "Topic: " + recordMetadata.topic() + "\n" +
+                            "Partition: " + recordMetadata.partition() + "\n" +
+                            "Offset: " + recordMetadata.offset() + "\n" +
+                            "Timestamp: " + recordMetadata.timestamp());
+//                    recordMetadata.
+                } else {
+
+                }
+            }
+        });
+        producer.flush();
+        producer.close();
+    }
+}
